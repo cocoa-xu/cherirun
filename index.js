49e69b0e693edb99cc03d63ee5df898cfb330365
Cocoa <i@uwucocoa.moe>
2024-01-29 16:49:59 +0700

    it works

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ const pug = require('pug');
 const logger = require('pino')();
 const pino = require('pino-http')();
 const crypto = require('crypto');
+const { rateLimit } = require('express-rate-limit');
 
+const server_port = 3000;
 const partial_run_script = fs.readFileSync('./static/js/run.js', 'utf8');
-// const max_network_lose_seconds = 2 * 60;
 const max_session_seconds = 2 * 60 * 60;
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+let status = {
+  connected: 0,
+  active_session: 0
+};
+let latest_tag = 'v2024.01.05-f8b62f01';
 
+app.use('/run', limiter);
 app.use(express.static('static'));
 app.use(pino);
 app.set('view engine', 'pug');
@@ -34,7 +47,7 @@ check_configurations = (conf) => {
   }
   if (['latest', 'v2024.01.05-f8b62f01'].includes(version) == false) {
     error_message += `error: invalid version: ${version}, will use default version \`latest\`\n`;
-    version = 'latest';
+    version = latest_tag;
   }
   let docker_image_name = '';
   if (disk_image == 'normal') {
@@ -104,18 +117,31 @@ io.on('connection', (socket) => {
   const address = socket.handshake.address;
   const user = createShortHash(address);
 
-  logger.info(`[${user}@${address}] socketio connected`);
-  let term = undefined;
+  logger.info({user, address, event: "websocket_connected"});
+  status.connected += 1;
+
   socket.on('request_start', (conf) => {
+    let term = undefined;
+    let session_time_handle = undefined;
+
+    socket.on('disconnect', () => {
+      status.connected -= 1;
+      logger.info({user, address, event: "websocket_disconnected"});
+      term && term.write(Uint8Array.from([0x01, 'x'.charCodeAt(0)]));
+      term && term.kill(9);
+      session_time_handle && clearInterval(session_time_handle);
+      session_time_handle = undefined;
+    });
+
     if (conf == {}  || conf === undefined || (conf.architecture === undefined && conf.disk_image === undefined && conf.version === undefined)) {
-      logger.error(`[${user}@${address}] missing parameters in configuration`);
+      logger.error({user, address, event: "sanity_check_error", msg: "missing parameters in configuration"});
       socket.emit('request_error', 'error: missing parameters in configuration');
       socket.disconnect();
       return;
     }
     const [docker_image_name, error_message, _arch, _disk_image, _version, _code, _run_url] = check_configurations(conf);
     if (error_message != '') {
-      logger.error(`[${user}@${address}] invalid configuration`);
+      logger.error({user, address, event: "sanity_check_error", msg: "invalid configuration"});
       socket.emit('request_error', 'error: invalid configuration');
       socket.disconnect();
       return;
@@ -123,25 +149,23 @@ io.on('connection', (socket) => {
 
     const session_start = Date.now();
     let session_time = 0;
-    // term = pty.spawn('docker', [
-    //   'run', '-it', '--rm', 
-    //   '--cpus=2', '--memory=1024m', 
-    //   // '--storage-opt', 'size=16G', 
-    //   // '--device-read-bps', '50mb',
-    //   // '--device-write-bps', '50mb',
-    //   docker_image_name], {
-    term = pty.spawn('bash', [], {
+    term = pty.spawn('docker', [
+      'run', '-it', '--rm', 
+      '--cpus=2', '--memory=1024m', 
+      docker_image_name], {
       name: 'xterm-color',
       cols: 80,
       rows: 24,
       cwd: process.env.HOME,
       env: process.env,
     });
+    logger.info({user, address, event: "pty_spawn", docker_image: docker_image_name});
+    status.active_session += 1;
     session_time_handle = setInterval(() => {
       session_time = Math.floor((Date.now() - session_start) / 1000);
       socket.emit('session_time', { session_start: session_start, session_time: session_time, max_session_seconds: max_session_seconds });
       if (session_time > max_session_seconds) {
-        logger.info(`[${user}@${address}] max session time reached`);
+        logger.info({user, address, event: "session_timeup", session_time: session_time});
         socket.disconnect();
         session_time_handle && clearInterval(session_time_handle);
         session_time_handle = undefined;
@@ -151,26 +175,26 @@ io.on('connection', (socket) => {
       socket.emit('data', data);
     });
     socket.on('data', (data) => {
-      term.write(data);
+      term && term.write(data);
     });
     socket.on('resize', (data) => {
-      term.resize(data.cols, data.rows);
-    });
-    socket.on('disconnect', () => {
-      logger.info(`[${user}@${address}] socketio disconnected`);
-      term.write(Uint8Array.from([0x01, 'x'.charCodeAt(0)]));
-      term.kill(9);
-      session_time_handle && clearInterval(session_time_handle);
-      session_time_handle = undefined;
+      term && term.resize(data.cols, data.rows);
     });
     term.onExit(e => {
-      logger.info(`[${user}@${address}] processes exited, code=${e.exitCode}, signal=${e.signal}`);
+      session_time = Math.floor((Date.now() - session_start) / 1000);
+      logger.info({user, address, event: "term_exit", code: e.exitCode, signal: e.signal, session_time: session_time});
       socket.emit('Process exited with code ' + e.exitCode);
+      status.active_session -= 1;
+      term = undefined;
       socket.disconnect();
     });
   });
 });
 
-server.listen(3000, () => {
-  logger.info(`listening on *:3000`);
+setInterval(() => {
+  logger.info(status)
+}, 30000);
+
+server.listen(server_port, () => {
+  logger.info({event: 'server_start', port: server_port});
 });
